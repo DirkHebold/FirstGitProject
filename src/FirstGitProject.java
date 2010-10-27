3f84edb1c8d8fbd3aa6d45c414af401a5c096b55
DirkHebold <dirk.hebold@hebinet.de>
2010-10-27 23:03:56 +0200

    Die Welt ist nicht genug...

--- src/FirstGitProject.java
+++ src/FirstGitProject.java
@@ -12,7 +12,7 @@ public class FirstGitProject {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello Universe");
 	}
 
 }
