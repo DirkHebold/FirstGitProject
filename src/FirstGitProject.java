7fb431559e5ce5d89b60cbd92f8f08c03acd2a52
DirkHebold <dirk.hebold@hebinet.de>
2010-10-27 22:51:10 +0200

    Erster Wurf

---
+++ src/FirstGitProject.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author DirkHebold
+ *
+ */
+public class FirstGitProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
